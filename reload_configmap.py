b63bcc3050257d2fd32a22001aabc6853ef2fc21
youssefelbiyadi <50877390+youssefelbiyadi@users.noreply.github.com>
2023-08-24 16:31:32 +0200

    Update reload_configmap.py

--- reload_configmap.py
+++ reload_configmap.py
@@ -1,13 +1,15 @@
 import os
 import time
-import argparse
 from kubernetes import client, config, watch
 from subprocess import call
 
-def reload_prometheus(namespace, prometheus_labels):
-    v1 = client.CoreV1Api()
+def reload_prometheus():
+    # Replace with your Kubernetes namespace and Prometheus pod labels
+    namespace = "your-namespace"
+    prometheus_labels = "app=prometheus"
 
     # Get the Prometheus pod name
+    v1 = client.CoreV1Api()
     pod_list = v1.list_namespaced_pod(namespace, label_selector=prometheus_labels)
     pod_name = pod_list.items[0].metadata.name
 
@@ -15,24 +17,21 @@ def reload_prometheus(namespace, prometheus_labels):
     call(["kubectl", "-n", namespace, "exec", pod_name, "--", "/bin/kill", "-HUP", "1"])
 
 def main():
-    parser = argparse.ArgumentParser(description="Watch a ConfigMap for changes and trigger Prometheus reload.")
-    parser.add_argument("namespace", help="Kubernetes namespace")
-    parser.add_argument("configmap", help="ConfigMap name")
-    parser.add_argument("labels", help="Prometheus pod labels in 'key=value' format")
-
-    args = parser.parse_args()
-
     # Load Kubernetes configuration
     config.load_incluster_config()
+
+    # Replace with your ConfigMap name and namespace
+    config_map_name = "prometheus-config"
+    namespace = "your-namespace"
 
     # Watch for changes in the ConfigMap
     v1 = client.CoreV1Api()
     w = watch.Watch()
-    for event in w.stream(v1.list_namespaced_config_map, args.namespace):
+    for event in w.stream(v1.list_namespaced_config_map, namespace):
         config_map = event["object"]
-        if config_map.metadata.name == args.configmap:
+        if config_map.metadata.name == config_map_name:
             print("Detected ConfigMap change. Reloading Prometheus configuration...")
-            reload_prometheus(args.namespace, args.labels)
+            reload_prometheus()
         time.sleep(1)
 
 if __name__ == "__main__":
